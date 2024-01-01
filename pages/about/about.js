525ca668fe0d5f71ba6dc4381c1c15dd7c6c34b0
comproterreno <comproterreno4@gmail.com>
2024-01-01 13:49:57 -0800

    styling

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,2 +1,30 @@
-/*
-*/
+
+// pages/about/about.js
+
+import React from 'react';
+import Layout from '/layouts/layout';
+
+const AboutPage = () => {
+  return (
+    <Layout>
+    <div>
+      <h1>Sobre Nosotros</h1>
+      <p>
+        En ComproTerreno actuamos como intermediarios para facilitar la venta de tu terreno al mejor comprador. Nos especializamos en conectar propietarios de terrenos con personas interesadas en adquirirlos, creando una experiencia sin complicaciones para ambas partes.
+      </p>
+      <p>
+        ¿Conoces a alguien que tiene un terreno? ¡Comparte esa información con nosotros y gana una comisión! En ComproTerreno valoramos la colaboración y recompensamos tu ayuda para expandir nuestra red de propiedades disponibles.
+      </p>
+      <p>
+        Nuestra misión es simplificar el proceso de compraventa de terrenos, proporcionando un servicio eficiente y transparente. Contamos con un equipo dedicado que trabaja para asegurar que cada transacción sea beneficiosa para vendedores y compradores por igual.
+      </p>
+      <p>
+        Gracias por confiar en ComproTerreno. Estamos aquí para ayudarte en cada paso del camino.
+      </p>
+      
+    </div>
+    </Layout>
+  );
+};
+
+export default AboutPage;
