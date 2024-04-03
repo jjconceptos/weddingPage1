4974e41b625f9f54f1215225974729036124488b
jjconceptos <jjconceptos21@gmail.com>
2024-04-03 08:52:42 -0600

    “logo”

--- pages/home/home.js
+++ pages/home/home.js
@@ -399,7 +399,7 @@ const LandingPage = () => {
 
           .container-three {
             position: absolute;
-          margin-top: 883vh;
+          margin-top: 903vh;
           left: 50%;
           transform: translate(-50%, -50%);
           text-align: center;
