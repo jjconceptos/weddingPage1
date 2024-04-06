a9c1d5a20ab8255d8edf9367b9c6ac5ab47c5442
jjconceptos <jjconceptos21@gmail.com>
2024-04-05 19:15:14 -0600

    “logo”

--- pages/home/home.js
+++ pages/home/home.js
@@ -17,7 +17,7 @@ const LandingPage = () => {
     { name: 'Itinerario', scrollPosition: 1475 },
     { name: 'Hospedaje', scrollPosition: 3470 },
     { name: 'Mesa de regalos', scrollPosition: 9600 },
-    { name: 'Dresscode', scrollPosition: 10100 },
+    { name: 'Dresscode', scrollPosition: 10200 },
     // Add more sections as needed
   ];
 
