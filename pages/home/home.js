f07fb5c739a7d9def05186318d0b0a8ba6032633
jjconceptos <jjconceptos21@gmail.com>
2024-04-14 22:46:56 -0600

    “logo”

--- pages/home/home.js
+++ pages/home/home.js
@@ -1049,7 +1049,7 @@ const LandingPage = () => {
             
             .container-three.chrome {
               /* Chrome-specific styles */
-              height: calc(var(--presentation-card-height) * 100); /* Set height based on custom property */
+              height: calc(var(--presentation-card-height) * 700); /* Set height based on custom property */
             }
   
             .hosting {
