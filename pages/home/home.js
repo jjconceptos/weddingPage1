4361234655935f2fe8aeb2074b60d6447e5688bc
jjconceptos <jjconceptos21@gmail.com>
2024-04-18 11:05:02 -0600

    “logo”

--- pages/home/home.js
+++ pages/home/home.js
@@ -1072,7 +1072,7 @@ const LandingPage = () => {
             
             .container-three.chrome {
               /* Chrome-specific styles */
-              margin-top: 1214%;           }
+              margin-top: 1114%;           }
   
             .hosting {
               position: relative;
